341066cf31fc15babe3275d90d47197abfe6a45a
AviatorRob <aviator.rob06@gmail.com>
2024-12-02 18:05:08 -0500

    remove remnants of old YouTube source library

--- common/src/main/java/dev/lavalink/youtube/OptionDisabledException.java
+++
@@ -1,7 +0,0 @@
-package dev.lavalink.youtube;
-
-public class OptionDisabledException extends RuntimeException {
-    public OptionDisabledException(String message) {
-        super(message, null, true, false);
-    }
-}
