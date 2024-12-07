ae0665b536e57a80e407b0ac94680218afe2e2ea
AviatorRob <aviator.rob06@gmail.com>
2024-12-06 21:27:13 -0500

    change: version numbers updated for next build

--- common/src/main/java/xyz/ar06/disx/DisxModInfo.java
+++ common/src/main/java/xyz/ar06/disx/DisxModInfo.java
@@ -12,7 +12,7 @@ import java.net.http.HttpResponse;
 import java.util.HashMap;
 
 public class DisxModInfo {
-    private static final String VERSION = "0.1.4";
+    private static final String VERSION = "0.1.3";
     private static final boolean DEV_BUILD = false;
     private static String LATEST_VERSION = "N/A - NO INTERNET";
     private static final String DISCORD_URL = "http://discord.ar06.xyz";
