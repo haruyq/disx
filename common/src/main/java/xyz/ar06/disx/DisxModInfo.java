863370309f3bd2cbffe9b420138ebe25de535c40
AviatorRob <aviator.rob06@gmail.com>
2025-03-12 04:37:53 -0400

    changed: removed client only annotation on DisxAudioInstance and DisxAudioInstanceRegistry to allow proper mixin registration on Fabric server, and to ensure that it doesn't crash on start up

--- common/src/main/java/xyz/ar06/disx/DisxModInfo.java
+++ common/src/main/java/xyz/ar06/disx/DisxModInfo.java
@@ -12,9 +12,9 @@ import java.net.http.HttpResponse;
 import java.util.HashMap;
 
 public class DisxModInfo {
-    private static final String VERSION = "0.3.0-dev-83e83f4e";
-    private static final boolean DEV_BUILD = true;
-    private static final boolean FORCE_DEBUG = true;
+    private static final String VERSION = "0.3.0";
+    private static final boolean DEV_BUILD = false;
+    private static final boolean FORCE_DEBUG = false;
     private static final String[] debugKeys = new String[]{
             "skibidi gooning",
             "uninstall brainrot.exe"
